baf3b07fd73097f3bae08d524e41d33e137a2580
murilo ijanc <mbsd@m0x.ru>
2025-02-10 20:40:10 -0300

    chore: add tracing

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ static ALLOC: Jemalloc = Jemalloc;
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
+    tracing_subscriber::fmt::init();
 
     println!("Hello, world!");
 
