9a7672fedfb6ec01e33b9cccfc7c72a22bf6d528
murilo ijanc <mbsd@m0x.ru>
2025-02-10 20:37:29 -0300

    chore: add tokio, color_eyre

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ use tikv_jemallocator::Jemalloc;
 #[global_allocator]
 static ALLOC: Jemalloc = Jemalloc;
 
-fn main() {
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
     println!("Hello, world!");
+
+    Ok(())
 }
