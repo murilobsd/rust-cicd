277bef76513530c2cec47b16988df8f71eb5fa6d
murilo ijanc <mbsd@m0x.ru>
2025-02-10 20:57:10 -0300

    chore: add axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
 use tikv_jemallocator::Jemalloc;
 
 #[global_allocator]
@@ -9,7 +15,26 @@ async fn main() -> color_eyre::Result<()> {
     _ = dotenvy::dotenv();
     tracing_subscriber::fmt::init();
 
-    println!("Hello, world!");
+    run().await?;
 
     Ok(())
+}
+
+async fn run() -> color_eyre::Result<()> {
+    let app = Router::new()
+        .route("/health", get(health))
+        .route("/", get(handler));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    tracing::info!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await?;
+    Ok(())
+}
+
+async fn health() -> impl IntoResponse {
+    StatusCode::OK
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
